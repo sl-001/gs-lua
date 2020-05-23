9e7ce5523fa71c4c07612e3bdddf708dfe944855
solo <50339924+solohvh@users.noreply.github.com>
2020-05-23 18:36:00 +0200

    fixed overflow, edited the code to not look cancer

--- cool_line.lua
+++ cool_line.lua
@@ -1,20 +1,19 @@
-local x, y = client.screen_size()
-local gui =
-{
-    checkbox = ui.new_checkbox("visuals", "effects", "Cool line"),
-	color = ui.new_color_picker("visuals", "effects", "Rainbow line", 255, 255, 255, 255),
-		types = ui.new_combobox("visuals", "effects", "\n", "Gradient", "Static", "Overflow"),
-	thickness = ui.new_slider("visuals", "effects", "\n", 1, 5, 1, true, "px", 1)
+local w, h = client.screen_size( )
+local int = {
+    checkbox = ui.new_checkbox( "visuals", "effects", "Cool line" ),
+	color = ui.new_color_picker( "visuals", "effects", "Rainbow line", 255, 255, 255, 255 ),
+	types = ui.new_combobox( "visuals", "effects", "\n", "Gradient", "Static", "Overflow" ),
+	thickness = ui.new_slider( "visuals", "effects", "\n", 1, 5, 1, true, "px", 1 )
 }
 
-local function hsv_to_rgb(h, s, v, a)
+local function hsv_to_rgb( h, s, v, a )
     local r, g, b
 
-    local i = math.floor(h * 6);
+    local i = math.floor( h * 6 );
     local f = h * 6 - i;
-    local p = v * (1 - s);
-    local q = v * (1 - f * s);
-    local t = v * (1 - (1 - f) * s);
+    local p = v * ( 1 - s );
+    local q = v * ( 1 - f * s );
+    local t = v * ( 1 - ( 1 - f ) * s );
 
     i = i % 6
 
@@ -29,8 +28,8 @@ local function hsv_to_rgb(h, s, v, a)
     return r * 255, g * 255, b * 255, a * 255
 end
 
-local function func_rgb_rainbowize(frequency, rgb_split_ratio)
-    local r, g, b, a = hsv_to_rgb(globals.realtime() * frequency, 1, 1, 1)
+local function func_rgb_rainbowize( frequency, rgb_split_ratio )
+    local r, g, b, a = hsv_to_rgb( globals.realtime( ) * frequency, 1, 1, 1 )
 
     r = r * rgb_split_ratio
     g = g * rgb_split_ratio
@@ -39,47 +38,33 @@ local function func_rgb_rainbowize(frequency, rgb_split_ratio)
     return r, g, b
 end
 
-client.set_event_callback("paint", function(ctx)
-local r, g, b = func_rgb_rainbowize(0.1, 1)
-local rs, gs, bs, as = ui.get(gui.color)
+local function menu( )
+    ui.set_visible( int.thickness, ui.get( int.checkbox ) )
+    ui.set_visible( int.types, ui.get( int.checkbox ) )
+    ui.set_visible( int.color, ui.get( int.checkbox ) and ui.get( int.types ) == "Static" )
+end
 
-    if ui.get(gui.checkbox) then
-        ui.set_visible(gui.thickness, true)
-		ui.set_visible(gui.types, true)
-		if ui.get(gui.types) == "Static" then
-		    ui.set_visible(gui.color, true)
-	    else
-		    ui.set_visible(gui.color, false)
-		end
-    else
-        ui.set_visible(gui.thickness, false)
-		ui.set_visible(gui.types, false)
-		ui.set_visible(gui.color, false)
-    end
+ui.set_callback( int.checkbox, menu )
+ui.set_callback( int.types, menu )
+menu( )
 
-    if ui.get(gui.checkbox) then
-	    if ui.get(gui.types) == "Gradient" then
-		
-	        renderer.gradient(0, 0, x/2, ui.get(gui.thickness), 5, 221, 255, 255, 186, 12, 230, 255, true)
-            renderer.gradient(x/2, 0, x, ui.get(gui.thickness), 186, 12, 230, 255, 219, 226, 60, 255, true)
-			
-		elseif ui.get(gui.types) == "Static" then
-		
-		    renderer.rectangle(0, 0, x/2, ui.get(gui.thickness), rs, gs, bs, 255)
-			renderer.rectangle(x/2, 0, x, ui.get(gui.thickness), rs, gs, bs, 255)
-			
-	    elseif ui.get(gui.types) == "Overflow" then
-		
+client.set_event_callback( "paint", function( )
+local r, g, b, a = ui.get( int.color )
+local ro, go, bo = func_rgb_rainbowize( 0.1, 1 )
+
+    if ui.get( int.checkbox ) then
+	    if ui.get( int.types ) == "Gradient" then
+	        renderer.gradient( 0, 0, w/2, ui.get( int.thickness ), 5, 221, 255, 255, 186, 12, 230, 255, true )
+            renderer.gradient( w/2, 0, w, ui.get( int.thickness ), 186, 12, 230, 255, 219, 226, 60, 255, true )
+		elseif ui.get( int.types ) == "Static" then
+		    renderer.rectangle( 0, 0, w, ui.get( int.thickness ), r, g, b, 255 )
+	    elseif ui.get( int.types ) == "Overflow" then
 		    local a = 255
-			
-		    renderer.gradient(0, 0, x/2, ui.get(gui.thickness), g, b, r, a, r, g, b, a, true)
-		    renderer.gradient(x/2, 0, x, ui.get(gui.thickness), r, g, b, a, b, r, g, a, true)
-
+		    renderer.gradient( 0, 0, w/2, ui.get( int.thickness ), go, bo, ro, a, ro, go, bo, a, true )
+		    renderer.gradient( w/2, 0, w/2, ui.get( int.thickness ), ro, go, bo, a, bo, ro, go, a, true )
 		    local a_lower = a*0.5
-		
-		    renderer.gradient(0, 0, x/2, ui.get(gui.thickness), g, b, r, a_lower, r, g, b, a_lower, true)
-		    renderer.gradient(x, 0, x, ui.get(gui.thickness), r, g, b, a_lower, b, r, g, a_lower, true)
-			
+		    renderer.gradient( 0, 0, w/2, ui.get( int.thickness ), go, bo, ro, a_lower, ro, go, bo, a_lower, true )
+		    renderer.gradient( w/2, 0, w/2, ui.get( int.thickness ), ro, go, bo, a_lower, bo, ro, go, a_lower, true )
 		end
 	end
-end)
+end )
