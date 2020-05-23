9fc41fd32b80413266f063213461aa40859278da
solo <50339924+solohvh@users.noreply.github.com>
2020-05-23 18:15:41 +0200

    added on-shot, head height, ping spike

--- indicators.lua
+++ indicators.lua
@@ -1,8 +1,8 @@
 -- cache common functions
 local renderer = renderer
 local line, rectangle, text = renderer.line, renderer.rectangle, renderer.text
-local get_screen_size = client.screen_size
-local get_local_player, get_prop = entity.get_local_player, entity.get_prop
+local get_screen_size, get_latency = client.screen_size, client.latency
+local get_local_player, get_prop, get_hitboxpos, get_player_resource = entity.get_local_player, entity.get_prop, entity.hitbox_position, entity.get_player_resource
 local min, max, abs, sqrt, floor = math.min, math.max, math.abs, math.sqrt, math.floor
 local get, get_mousepos = ui.get, ui.mouse_position
 local band = bit.band
@@ -13,20 +13,22 @@ local scr = { w = wdei, h = hdei }
 local menu_color = ui.reference( "misc", "settings", "menu color" )
 local rm, gm, bm, am = ui.get( menu_color )
 local choked_ticks = 0
-local old_origin = { x=180, y=180, z=170 }
+local old_origin = { x=0, y=0, z=0 }
 local old_simulation_time = 0
 local fdk = ui.reference( "rage", "other", "duck peek assist" )
 local dtap, dtk = ui.reference( "rage", "other", "double tap" )
 local fl_am = ui.reference( "aa", "fake lag", "limit" )
 local aa_enabled = ui.reference( "aa", "anti-aimbot angles", "enabled" )
+local onshot, onshkey = ui.reference( "aa", "other", "on shot anti-aim" )
+local pings, pingsk = ui.reference( "misc", "miscellaneous", "ping spike" )
 local byaw = ui.reference( "aa", "anti-aimbot angles", "body yaw" )
-local max_choked_ticks = ui.reference( "MISC", "Settings", "sv_maxusrcmdprocessticks" )
+local max_choked_ticks = ui.reference( "misc", "settings", "sv_maxusrcmdprocessticks" )
 local angle = 0
 
 local int = {
     enabled = ui.new_checkbox( "visuals", "other esp", "Indicators" ),
     color = ui.new_color_picker( "visuals", "other esp", "otindc_color", rm, gm, bm, am ),
-    options = ui.new_multiselect( "visuals", "other esp", "\n", "Fakelag", "Lag compensation", "Double tap", "Fake duck", "Fake"),
+    options = ui.new_multiselect( "visuals", "other esp", "\n", "Fakelag", "Lag compensation", "Double tap", "Fake duck", "Fake", "On-shot", "Head height", "Ping spike"),
     statuss = ui.new_combobox( "visuals", "other esp", "Indication type", "Color", "Checkmark" ),
     max_bar = ui.new_checkbox( "visuals", "other esp", "Maximum bar mode" )
 }
@@ -36,11 +38,14 @@ local wnd = {
     dragging = false
 }
 local heights = {
-    ["Fakelag"] = 18,
+    ["Fakelag"] = 20,
     ["Lag compensation"] = 18,
     ["Fake duck"] = 18,
     ["Double tap"] = 18,
-    ["Fake"] = 25
+    ["Fake"] = 25,
+    ["On-shot"] = 18,
+    ["Head height"] = 25,
+    ["Ping spike"] = 18
 }
 
 local function tointeger( n )
@@ -89,13 +94,13 @@ local function conta( x, y, w, h )
 end
 local function rendind( x, y, max, value, size, r, g, b, a, name )
     if value > 1 then value = value + 1 end
-    if value > max then value = max end
+    if value+1 > max then value = max+1 end
     for i=1, max do
         rectangle( x+( size+2 )*i-( size+2 ), y+6, size, 5, 15, 15, 15, 150 )
     end
     for i=1, value do
         if i < value then i = i+1 end
-        rectangle( x+( size+2 )*i-( size+2 )*2, y+6, size, 5, r, g, b, 180 )
+        rectangle( x+( size+2 )*i-( size+2 )*2, y+6, size, 5, r, g, b, 180)
     end
     if name ~= "" and name ~= " " and name ~= nil then
         text( x, name == string.upper( name ) and y-8 or y-10, 255, 255, 255, 255, name == string.upper( name ) and "-" or "", 0, name )
@@ -142,14 +147,25 @@ local function on_paint( )
     local left_click = client.key_state( 0x01 )
     local cx, cy = get_mousepos( )
     local options = ui.get( int.options )
-    local width, height = 116, 10
+    local width, height = 116, 8
 
+    local curr_ping = get_prop( get_player_resource( ), "m_iPing", get_local_player( ) )
     local origin = vec_3( get_prop( get_local_player( ), "m_vecOrigin" ) )
     local velocity_prop = vec_3( entity.get_prop( get_local_player( ), "m_vecVelocity" ) )
     local velocity = sqrt( velocity_prop.x * velocity_prop.x + velocity_prop.y * velocity_prop.y )
     local trace_fraction, trace_entity = client.trace_line( 1, origin.x, origin.y, origin.z, origin.x, origin.y, origin.z - 24.97 )
-    local bar_max, bar_fake = 9, tointeger( angle )/6.666666666666667
-    local bar_ticks = tointeger( choked_ticks/1.5 )
+    local _, _, orig_z = get_prop( get_local_player( ), "m_vecAbsOrigin" )
+    local _, _, head_z = get_hitboxpos( get_local_player( ), 0 )
+    if orig_z ~= nil and head_z ~= nil then
+        if head_z ~= head_z_old then
+            head_z_old = head_z
+            local duck_am = get_prop( get_local_player( ), "m_flDuckAmount" ) or 0
+        end
+    end
+    local headp_delta = head_z - orig_z + ( duckam or 0 )*12
+    local h_min, h_max = 55, 70
+    local headp = max( 0, min( 1, 1-( headp_delta-h_min )/( headp_delta-h_max ) ) )
+    local bar_max, bar_fake, bar_headh, bar_ticks = 9, tointeger( angle )/6.666666666666667, floor( 9*headp ), tointeger( choked_ticks/1.5 )
     local lc = { r = 255, g = 0, b = 0, state = false }
     local dt = { r = 255, g = 0, b = 0, state = false }
     local fk = { r = 255, g = 0, b = 0 }
@@ -215,6 +231,7 @@ local function on_paint( )
     if ui.get( int.max_bar ) then 
         bar_ticks = choked_ticks
         bar_fake = angle/4.285714285714286
+        bar_headh = floor( 14*headp )
         width = 176
         bar_max = 14
     end
@@ -248,6 +265,22 @@ local function on_paint( )
             end
         elseif option == "Fake" then
             render.indication( indic.x, indic.y + 6, bar_max, bar_fake, 10, fk.r, fk.g, fk.b, 255, string.format( "FAKE  -  %s", tointeger( angle ) ) )
+        elseif option == "On-shot" then
+            if ui.get( int.statuss ) == "Color" then
+                text( indic.x, indic.y, ui.get( aa_enabled ) and ui.get( onshot ) and ui.get( onshkey ) and r or 255, ui.get( aa_enabled ) and ui.get( onshot ) and ui.get( onshkey ) and g or 0, ui.get( aa_enabled ) and ui.get( onshot ) and ui.get( onshkey ) and b or 0, 255, "-", 0, "ON-SHOT" )
+            else
+                text( indic.x, indic.y, 255, 255, 255, 255, "-", 0, "ON-SHOT" )
+                render.status( indic.x+width-15, indic.y+8, 5, ui.get( aa_enabled ) and ui.get( onshot ) and ui.get( onshkey ) )
+            end
+        elseif option == "Head height" then
+            render.indication( indic.x, indic.y + 6, bar_max, bar_headh, 10, r, g, b, 255, "HEAD HEIGHT" )
+        elseif option == "Ping spike" then
+            if ui.get( int.statuss ) == "Color" then
+                text( indic.x, indic.y, ui.get( pings ) and ui.get( pingsk ) and curr_ping >= 100 and r or 255, ui.get( pings ) and ui.get( pingsk ) and curr_ping >= 100 and g or 0, ui.get( pings ) and ui.get( pingsk ) and curr_ping >= 100 and b or 0, 255, "-", 0, "PING" )
+            else
+                text( indic.x, indic.y, 255, 255, 255, 255, "-", 0, "PING" )
+                render.status( indic.x+width-15, indic.y+8, 5, ui.get( pings ) and ui.get( pingsk ) and curr_ping >= 100 )
+            end
         end
         if heights[option] ~= nil then
             indic.y = indic.y + ( heights[ option ]-1 )
