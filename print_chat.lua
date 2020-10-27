fe5bf8da2ed855240efc77b12aacefec9e0376e8
solo <50339924+sl-001@users.noreply.github.com>
2020-10-27 15:26:47 +0100

    Create print_chat.lua

---
+++ print_chat.lua
@@ -0,0 +1,20 @@
+local ffi = require("ffi")
+ffi.cdef[[
+typedef void***(__thiscall* FindHudElement_t)(void*, const char*);
+typedef void(__cdecl* ChatPrintf_t)(void*, int, int, const char*, ...);
+]]
+local signature_gHud = "\xB9\xCC\xCC\xCC\xCC\x88\x46\x09"
+local signature_FindElement = "\x55\x8B\xEC\x53\x8B\x5D\x08\x56\x57\x8B\xF9\x33\xF6\x39\x77\x28"
+local match = client.find_signature("client_panorama.dll", signature_gHud) or error("sig1 not found")
+local hud = ffi.cast("void**", ffi.cast("char*", match) + 1)[0] or error("hud is nil")
+match = client.find_signature("client_panorama.dll", signature_FindElement) or error("FindHudElement not found")
+local find_hud_element = ffi.cast("FindHudElement_t", match)
+local hudchat = find_hud_element(hud, "CHudChat") or error("CHudChat not found")
+local chudchat_vtbl = hudchat[0] or error("CHudChat instance vtable is nil")
+local print_to_chat = ffi.cast("ChatPrintf_t", chudchat_vtbl[27])
+local function print_chat(text)
+	print_to_chat(hudchat, 0, 0, text)
+end
+return { 
+	print_ = print_chat 
+}
