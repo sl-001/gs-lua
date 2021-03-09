a79dc06d35ace3269883aa31fe60b9421b11762f
solo <50339924+sl-001@users.noreply.github.com>
2021-03-09 21:38:21 +0100

    Update off_screen.lua

--- off_screen.lua
+++ off_screen.lua
@@ -1,19 +1,36 @@
-local vector = require "vector"
+--» init gs libraries
+local vector, images = require "vector", require "gamesense/images"
 
+local gs = {ent = entity, render = renderer, client = client}
+
+--» variables
 local w, h = client.screen_size()
-local off = {
-    enabled = ui.new_checkbox("lua", "a", "Player indication"),
-    clr = ui.new_color_picker("lua", "a", "arr:clr", 255, 255, 255, 255),
-    size = ui.new_slider("lua", "a", "\n", 1, 35, 16, true, "px"),
-    radius = ui.new_slider("lua", "a", "\n\n", -1, 100, -1, true, "%", 1, {[-1]="Distance"}),
-    pulse = ui.new_slider("lua", "a", "Pulse", 0, 100, 18, true, "", 0.01, {[0]="Off"}),
-    off_ = ui.new_checkbox("lua", "a", "Only off-screen"),
-    lock = { ui.new_checkbox("lua", "a", "Lock on screen"), ui.new_slider("lua", "a", "\n\n\n", 0, 100, 80, true, "px") }
+local pa = {
+    enabled = ui.new_checkbox("visuals", "player esp", "Arrow indicators"),
+    clr = ui.new_color_picker("visuals", "player esp", "player_arrows:clr", 255, 255, 255, 255),
+    clr2 = ui.new_color_picker("visuals", "player esp", "player_arrows:clr2", 255, 0, 0, 255),
+    options = ui.new_multiselect("visuals", "player esp", "\noptions", "Dormant players", "Include weapons", "Include name", "Grenade warning", "Distance based size", "Distance based radius"),
+    size = ui.new_slider("visuals", "player esp", "\nsize", 1, 35, 15, true, "px"),
+    radius = ui.new_slider("visuals", "player esp", "\nradius", 0, 100, 100, true, "%"),
+    pulse = ui.new_slider("visuals", "player esp", "Pulse", 0, 100, 18, true, "", 0.01, {[0]="Off"}),
+    off_screen = ui.new_checkbox("visuals", "player esp", "Only off-screen"),
+    limit = ui.new_checkbox("visuals", "player esp", "Limit edges"),
+    limit_off = ui.new_slider("visuals", "player esp", "\nlimit_offset", 0, 100, 80, true, "px")
 }
+
+--» functions
+local function contains(tbl, val)
+	for i = 1, #tbl do
+		if tbl[i] == val then return true end
+	end
+	return false
+end
 
 local function lerp(a, b, percentage)
 	return a + (b - a) * percentage
 end
+--[[local alpha = globals.tickcount() % 510
+alpha = math.max(0, math.min(255, alpha))]]
 
 local function rotate_around_c(angle, center, point, point_)
     local s = math.sin(angle)
@@ -30,69 +47,132 @@ local function rotate_around_c(angle, center, point, point_)
     return xn+center.x, yn+center.y, x_n+center.x, y_n+center.y
 end
 
+--» callbacks
 local function on_paint()
-    local size, radius, clr = ui.get(off.size), 1000*(ui.get(off.radius)/100), {ui.get(off.clr)}
-    local fix_rad = radius+size
-    local realtime = globals.realtime() * (ui.get(off.pulse)/10)
-    local val = realtime % 2
-	if val > 1 then
-		val = 2 - val
-	end
-    local a_new = lerp(0, clr[4], val)
-    local local_pos = vector(entity.get_origin(entity.get_local_player())); local_pos.z = 0
-    if local_pos == nil then return end
-    local view = {client.camera_angles()}
-    if view == nil then return end
-    
-    for i=1, #entity.get_players(true) do
-        enemy = entity.get_players(true)[i]
-        
-        local pos = vector(entity.get_origin(enemy)); pos.z = 0
-        if pos == nil then return end
-        local distance = math.min(800, local_pos:dist(pos))
-        local w2s = {renderer.world_to_screen(pos.x, pos.y, pos.z)}
-        local angle_x, angle_y = local_pos:to(pos):angles()
-        if angle_y == nil then return end
-        angle_y = 270-angle_y+view[2]
-        local angle_rad = math.rad(angle_y)
-        if ui.get(off.radius) < 0 then fix_rad = (1000*distance/800)+size end
-        local point = { x=w/2+math.cos(angle_rad)*(fix_rad), y=h/2+math.sin(angle_rad)*(fix_rad) }
-        if ui.get(off.lock[1]) then
-            if point.x < ui.get(off.lock[2]) then point.x = ui.get(off.lock[2]) elseif point.x > w-ui.get(off.lock[2]) then point.x = w-ui.get(off.lock[2]) end
-            if point.y < ui.get(off.lock[2]) then point.y = ui.get(off.lock[2]) elseif point.y > h-ui.get(off.lock[2]) then point.y = h-ui.get(off.lock[2]) end
-        end
-        local point_, point2 = { x=point.x-size/2, y=point.y-size }, { x=point.x+size/2, y=point.y-size }
-        local get_rot = {rotate_around_c(math.rad(angle_y-90), point, point_, point2)}
-        local new_point, new_point_ = {get_rot[1], get_rot[2]}, {get_rot[3], get_rot[4]}
+    local player = gs.ent["get_local_player"]()
+    if not player and gs.ent["is_alive"](player) then return end
+    local r, g, b, a = ui.get(pa.clr)
+    local size, get_radius, opts = ui.get(pa.size), 10*ui.get(pa.radius), ui.get(pa.options)
+    local alpha = 1
+    if ui.get(pa.pulse) > 0 then
+        local realtime = globals.realtime() * (ui.get(pa.pulse)/10)
+        local val = realtime % 2
+	    if val > 1 then
+	    	val = 2 - val
+	    end
+        alpha = lerp(0, a, val)/255
+    end
+    local radius = get_radius+size
+    local view = vector(0, 0, 0)
+    view.x, view.y = client.camera_angles()
+    if view.x == nil then return end
+    local pos = vector(gs.ent["get_origin"](player))
+    local player_resource = gs.ent["get_player_resource"]()
+    if not pos then return end
 
-        if ui.get(off.off_) then
-            if not w2s[1] or w2s[1] > w or w2s[1] < 0 then
-                renderer.triangle(point.x, point.y, new_point[1], new_point[2], new_point_[1], new_point_[2], clr[1], clr[2], clr[3], ui.get(off.pulse) > 0 and a_new or clr[4])
+    for ent=1, globals.maxplayers() do
+        if gs.ent["get_prop"](player_resource, "m_bConnected", ent) ~= 1 then
+			goto skip
+		end
+
+        if not gs.ent["is_alive"](ent) then
+            goto skip
+        end
+
+        if not gs.ent["is_enemy"](ent) then
+            goto skip
+        end
+
+        local bb = {gs.ent["get_bounding_box"](ent)}
+        local position = vector(gs.ent["get_origin"](ent))
+        if not position then goto skip end
+        local dist = math.min(800, pos:dist(position))/800
+        local weapon = gs.ent["get_player_weapon"](ent)
+        local weapon_idx = gs.ent["get_prop"](weapon, "m_iItemDefinitionIndex")
+        if contains(opts, "Distance based size") then
+            size = size*(1-dist)
+        end
+        if contains(opts, "Distance based radius") then
+            radius = radius*(1-dist)
+        end
+        local w2s, w2s2 = gs.render["world_to_screen"](position.x, position.y, position.z)
+        local _, angle = pos:to(position):angles()
+        if not angle then goto skip end
+        angle = 270-angle+view.y
+        local angle_rad = math.rad(angle)
+        local point = vector(w/2+math.cos(angle_rad)*(radius), h/2+math.sin(angle_rad)*(radius), 0)
+
+        if ui.get(pa.limit) then
+            point.x = math.min(w-ui.get(pa.limit_off), math.max(ui.get(pa.limit_off), point.x))
+            point.y = math.min(h-ui.get(pa.limit_off), math.max(ui.get(pa.limit_off), point.y))
+        end
+
+        local point1, point2 = vector(point.x-size/2, point.y-size, 0), vector(point.x+size/2, point.y-size, 0)
+        local converted = {rotate_around_c(math.rad(angle-90), point, point1, point2)}
+
+        if gs.ent["is_dormant"](ent) then
+            if contains(opts, "Dormant players") then
+                r, g, b, a = 180, 180, 180, 255*bb[5]
+            else
+                goto skip
             end
+        end
+
+        if ui.get(pa.off_screen) then
+            if w2s and w2s2 and w2s2 > 0 and w2s2 < h then a = 0 end
+        end
+
+        if contains(opts, "Grenade warning") then
+            local class = gs.ent["get_classname"](ent)
+            if class:find("Grenade") then
+                r, g, b = ui.get(pa.clr2)
+                if not contains(opts, "Include weapons") then
+                    local get_icon = images.get_weapon_icon(weapon_idx)
+                    if get_icon then
+                        local def_size = get_icon:measure()
+                        local size = {get_icon:measure(def_size*0.5)}
+                        get_icon:draw(point.x-size[1]/2, point.y+5, size[1], size[2])
+                    end
+                end
+            end
+        end
+
+        gs.render["triangle"](point.x, point.y, converted[1], converted[2], converted[3], converted[4], r, g, b, a*alpha)
+        if contains(opts, "Include name") then
+            gs.render["text"](point.x, point.y, 220, 220, 220, a, "-c", 0, gs.ent["get_player_name"](ent):upper())
+        end
+        if contains(opts, "Include weapons") then
+            local get_icon = images.get_weapon_icon(weapon_idx)
+            if get_icon then
+                local def_size = get_icon:measure()
+                local size = {get_icon:measure(def_size*0.5)}
+                get_icon:draw(point.x-size[1]/2, point.y+5, size[1], size[2], 220, 220, 220, a)
+            end
+        end
+
+        ::skip::
+    end
+end
+
+local function ui_call(self)
+    local main = ui.get(pa.enabled)
+    local set_callback = gs.client[main and "set_event_callback" or "unset_event_callback"]
+
+    for name, i in pairs(pa) do
+        if name ~= "enabled" and name ~= "limit_off" and name ~= "pulse" then
+            ui.set_visible(i, main)
         else
-            renderer.triangle(point.x, point.y, new_point[1], new_point[2], new_point_[1], new_point_[2], clr[1], clr[2], clr[3], ui.get(off.pulse) > 0 and a_new or clr[4])
+            if name == "limit_off" then
+                ui.set_visible(i, main and ui.get(pa.limit))
+            elseif name == "pulse" then
+                ui.set_visible(i, main and not contains(ui.get(pa.options), "Distance based pulse"))
+            end
+            if self == pa.enabled then
+                set_callback("paint", on_paint)
+            end
         end
     end
 end
 
-local function set_call()
-    if ui.get(off.enabled) then
-        client.set_event_callback("paint", on_paint)
-    else
-        client.unset_event_callback("paint", on_paint)
-    end
-    ui.set_visible(off.size, ui.get(off.enabled))
-    ui.set_visible(off.radius, ui.get(off.enabled))
-    ui.set_visible(off.pulse, ui.get(off.enabled))
-    ui.set_visible(off.off_, ui.get(off.enabled))
-    ui.set_visible(off.lock[1], ui.get(off.enabled))
-    ui.set_visible(off.lock[2], ui.get(off.enabled) and ui.get(off.lock[1]))
-end
-ui.set_callback(off.enabled, set_call)
-set_call()
-
-local function menu_util()
-    ui.set_visible(off.lock[2], ui.get(off.enabled) and ui.get(off.lock[1]))
-end
-ui.set_callback(off.lock[1], menu_util)
-menu_util()
+ui.set_callback(pa.enabled, ui_call); ui.set_callback(pa.limit, ui_call); ui.set_callback(pa.options, ui_call)
+ui_call()
